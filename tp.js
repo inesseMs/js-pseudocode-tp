330cb22469ddb1811fe9f1d34cf745cbc19a2e4d
inesseMs <inessemohamedseghir@gmail.com>
2026-01-19 23:05:27 +0100

    message

---
+++ tp.js
@@ -0,0 +1,66 @@
+//Write pseudocode to display numbers from 1 to 10 using a loop.
+
+/* let i=0
+for (let i=0; i<=10; i++) {
+console.log(i);
+} */
+//-----------------------
+/* //Write pseudocode to display numbers from 10 to 1 using a loop.
+let i=10
+for (let i=10; i>=1; i--) {
+console.log(i);
+} */
+//-----------------------
+//Write pseudocode to calculate the sum of numbers from 1 to 50.
+/* let sum=0
+{for (let i=0; i<=50; i++)
+    sum = sum + i }
+
+console.log(sum); */
+//-----------------------
+//Write pseudocode to calculate the sum of even numbers from 1 to 20.
+/* let sum=0
+for (let i=1; i<=20; i++)
+    if (i%2===0) {
+    sum= sum +i 
+    console.log("even numbers");
+    }
+console.log("sum of even numbers is " + sum); */
+//-----------------------
+//Write pseudocode to find the largest of three numbers using conditions.
+/* let a=10
+    b= 5
+    c= 15
+if (a>b && a>c ) {
+    console.log("a is the largest");
+    
+} 
+else if (b>c && b>a)
+    { console.log("b is the largest");
+    }
+
+else {
+    console.log("c is the largest number");
+}
+     */
+//-----------------------
+//Write pseudocode to check whether a given number is positive, negative, or zero.
+/* let a=0
+if (a<0) {
+    console.log("a is negative");
+    }
+    else if (a>0)
+        console.log("a is positive");
+        
+    else {
+    console.log("a is zero");
+     }
+ */
+//-----------------------
+//Write pseudocode to calculate the factorial of a given number n.
+let n=10
+    resultat = n*n
+ for (let i=1; i>=10; i++) {
+resultat= i*n
+    }
+console.log("resultat"+ resultat);
